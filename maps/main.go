da3c5517e3ff65479b158e431cd92142df1ae315
Samarburnwal <samarburnwal@gmail.com>
2025-11-05 14:49:30 +0530

    learning maps

---
+++ maps/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	marks := make(map[string]int, 0)
+
+	marks["samar"] = 99
+	marks["rishav"] = 100
+	marks["aman"] = 99
+
+	for index, val := range marks {
+		fmt.Printf("Marks of %s is %d\n", index, val)
+	}
+
+	// checking the value existed previously or not
+	val, exist := marks["akash"]
+
+	fmt.Println(val, ", exist ? :", exist)
+	val1, exist1 := marks["akash"]
+	fmt.Println(val1, ", exist ? :", exist1)
+}
